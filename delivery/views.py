c853f0dec490ea7b97b635d35446d6c33a95ca02
Fabio Leal <sousaleal.fabio@gmail.com>
2011-10-05 03:00:55 -0300

    Testando primeiro commit no Git

--- delivery/views.py
+++ delivery/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from delivery.models import *
 
 def home(request):
     return render_to_response("home.html")
+
+def detalhar_catalogo_produtos(request, nome_cidade, nome_loja):
+#    return HttpResponse("Voce esta procurando pela cidade %s e pela loja %s. " % (nome_cidade,nome_loja))
+    produtos = Produto.objects.filter(catalogo__loja__nome=nome_loja, catalogo__loja__endereco__cidade=nome_cidade)
+    print produtos
+    return render_to_response("produtos.html", {"produtos" : produtos})
+
