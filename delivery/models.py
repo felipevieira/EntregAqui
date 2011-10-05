8ce3a48cf53a7efdb670c68b2bae8bd05297dbd0
Danilo Freitas <dsurviver@gmail.com>
2011-10-05 03:12:41 -0300

    Escolhendo cidade

--- delivery/models.py
+++ delivery/models.py
@@ -1,21 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Endereco(models.Model):
+    logradouro = models.CharField(max_length=200)
+    numero = models.IntegerField()
+    complemento = models.CharField(max_length=20, blank=True)
+    bairro = models.CharField(max_length=50)
+    cep = models.CharField(max_length=8)
+    cidade = models.CharField(max_length=50)
+    estado = models.CharField(max_length=50)
+    referencia = models.CharField(max_length=200, blank=True)
+
 class Usuario(models.Model):
     nome = models.CharField(max_length=50)
     email = models.CharField(max_length=30)
     cpf = models.CharField(max_length=11)
     senha = models.CharField(max_length=64)
-
-class Endereco(models.Model):
-    logradouro = models.CharField(max_length=200)
-    numero = models.IntegerField()
-    complemento = models.CharField(max_length=20)
-    bairro = models.CharField(max_length=50)
-    cep = models.CharField(max_length=8)
-    cidade = models.CharField(max_length=50)
-    estado = models.CharField(max_length=50)
-    referencia = models.CharField(max_length=200)
+    endereco = models.ForeignKey(Endereco)
 
 class Categoria(models.Model):
     nome = models.CharField(max_length=20)
@@ -33,7 +33,6 @@ class Produto(models.Model):
     descricao = models.CharField(max_length=200)
     preco = models.IntegerField()
     catalogo = models.ForeignKey(Catalogo, related_name="produtos")
-    endereco = models.ForeignKey(Endereco)
     
 class Funcionario(models.Model):
     nome = models.CharField(max_length=30)
--- delivery/views.py
+++ delivery/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from delivery.models import *
 
 def home(request):
-    return render_to_response("home.html")
+    enderecos = Endereco.objects.values('cidade').annotate()
+    return render_to_response("home.html", { 'enderecos': enderecos })
