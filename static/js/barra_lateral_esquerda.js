8fcf22d512d9cff96a1795686420f32e22ba8c81
Danilo Freitas <dsurviver@gmail.com>
2011-11-03 00:05:17 -0300

    Menu expansivel na barra lateral esquerda

---
+++ static/js/barra_lateral_esquerda.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $(".categoria > ul").hide();
+    $(".categoria").click(function(e) {
+        $(this).find("li").slideToggle("fast");
+        $(this).find("ul").show("fast");
+    })
+})
